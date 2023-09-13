9630035f62d7479175867859edcdaca12605749a
kamil <kamil_olszewski@op.pl>
2023-09-13 17:16:04 +0200

    del coment

--- robot_imu.py
+++ robot_imu.py
@@ -32,7 +32,7 @@ class ComplementaryFilter:
 
 
 class RobotImu:
-    """Definicja wspólnego interfejsu dla jednostki nawigacji inercyjnej z funkcją pomiaru temperatury"""
+
     def __init__(self, gyro_offsets=None,
                  mag_offsets=None):
         self._imu = ICM20948()
@@ -40,31 +40,30 @@ class RobotImu:
         self.mag_offsets = mag_offsets or vector(0, 0, 0)
 
     def read_temperature(self):
-        """Odczyt temperatury w stopniach C."""
+
         return self._imu.read_temperature()
 
     def read_gyroscope(self):
-        """ Pobranie przeskalowanych danych z żyroskopu"""
+
         _, _, _, x, y, z = self._imu.read_accelerometer_gyro_data()
         return vector(x, y, z) - self.gyro_offsets
 
     def read_accelerometer(self):
-        """Pobranie danych z akcelerometru"""
+
         accel_x, accel_y, accel_z, _, _, _ = self._imu.read_accelerometer_gyro_data()
         return vector(accel_x, accel_y, accel_z)
 
     def read_accelerometer_pitch_and_roll(self):
-        """Convert cartesian coordinates to spherical coordinates.
-        Work in degrees because the gyro does"""
+
         accel = self.read_accelerometer()
-        # pochylenie odbywa się wokół osi Y (użyj X i Z)
+
         pitch = degrees(-atan2(accel.x, accel.z))
-        # przechylenie odbywa się wokół osi X (użyj Y i Z)
+
         roll = degrees(atan2(accel.y, accel.z))
         return pitch, roll
 
     def read_magnetometer(self):
-        """Pobranie danych z magnetometru"""
+
         mag_x, mag_y, mag_z = self._imu.read_magnetometer_data()
         return vector(mag_x, -mag_y, -mag_z) - self.mag_offsets
 
@@ -80,14 +79,13 @@ class ImuFusion:
     def update(self, dt):
         accel_pitch, accel_roll = self.imu.read_accelerometer_pitch_and_roll()
         gyro = self.imu.read_gyroscope()
-        # Przez wyfiltrowanie 95% odczytów żyroskopu (które zmieniają sieę szybko) i 5% odczytów akcelerometru, które są bezwględne, ale wolne, gdy filtrowane,
-        # otrzymamy najlepsze wyniki dla obu czujników.
+
 
         self.pitch = self.filter(self.pitch + gyro.y * dt, accel_pitch)
         self.roll = self.filter(self.roll + gyro.x * dt, accel_roll)
-        # Odczyt magnetometru
+
         mag = self.imu.read_magnetometer()
-        # Korekta pochylenia i przechylenia
+
         mag = mag.rotate(radians(self.pitch), vector(0, 1, 0))
         mag = mag.rotate(radians(self.roll), vector(1, 0, 0))
 
