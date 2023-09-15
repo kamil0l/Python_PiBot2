4381083104e3cdc81df54c4cd561d9a91c3bab9f
kamil <kamil_olszewski@op.pl>
2023-09-15 10:05:47 +0200

    add behavior path to robot

--- robot.py
+++ robot.py
@@ -103,3 +103,33 @@ r.set_left(100)
 r.set_right(70)
 sleep(1)
 
+
+def straight(bot, seconds):
+    bot.set_left(100)
+    bot.set_right(100)
+    sleep(seconds)
+
+def turn_left(bot, seconds):
+    bot.set_left(20)
+    bot.set_right(80)
+    sleep(seconds)
+
+def turn_right(bot, seconds):
+    bot.set_left(80)
+    bot.set_right(20)
+    sleep(seconds)
+
+def spin_left(bot, seconds):
+    bot.set_left(-100)
+    bot.set_right(100)
+    sleep(seconds)
+
+bot = robot.Robot()
+straight(bot, 1)
+turn_right(bot, 0.6)
+straight(bot, 0.6)
+turn_left(bot, 0.6)
+straight(bot, 0.6)
+turn_left(bot, 0.6)
+straight(bot, 0.3)
+spin_left(bot, 1)
