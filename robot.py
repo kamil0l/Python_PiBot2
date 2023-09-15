ffb665d6cbf5afb37c84c8e42e057a05b7c75f46
kamil <kamil_olszewski@op.pl>
2023-09-15 10:11:01 +0200

    add calibrated gyro to robot

--- robot.py
+++ robot.py
@@ -133,3 +133,25 @@ straight(bot, 0.6)
 turn_left(bot, 0.6)
 straight(bot, 0.3)
 spin_left(bot, 1)
+
+
+imu = RobotImu()
+
+gyro_min = vp.vector(0, 0, 0)
+gyro_max = vp.vector(0, 0, 0)
+
+for n in range(500):
+    gyro = imu.read_gyroscope()
+    gyro_min.x = min(gyro_min.x, gyro.x)
+    gyro_min.y = min(gyro_min.y, gyro.y)
+    gyro_min.z = min(gyro_min.z, gyro.z)
+
+    gyro_max.x = max(gyro_max.x, gyro.x)
+    gyro_max.y = max(gyro_max.y, gyro.y)
+    gyro_max.z = max(gyro_max.z, gyro.z)
+
+    offset = (gyro_min + gyro_max) / 2
+
+    time.sleep(.01)
+
+print(f"Odchylenie od zera: {offset}.")
