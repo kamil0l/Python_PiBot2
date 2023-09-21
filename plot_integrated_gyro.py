4dd82791cac95a510c1289e45b1c4d7ba01a1b06
kamil <kamil_olszewski@op.pl>
2023-09-21 21:03:54 +0200

    ad plot integrated gyro

--- plot_integrated_gyro.py
+++ plot_integrated_gyro.py
@@ -29,3 +29,22 @@ while True:
     graph_x.plot(elapsed, pitch)
     graph_y.plot(elapsed, roll)
     graph_z.plot(elapsed, yaw)
+
+
+import vpython as vp
+from robot_imu import RobotImu
+from delta_timer import DeltaTimer
+import imu_settings
+
+imu = RobotImu(mag_offsets=imu_settings.mag_offsets)
+
+vp.graph(xmin=0, xmax=60, scroll=True)
+graph_yaw = vp.gcurve(color=vp.color.blue)
+timer = DeltaTimer()
+
+while True:
+    vp.rate(100)
+    dt, elapsed = timer.update()
+    mag = imu.read_magnetometer()
+    yaw = -vp.degrees(vp.atan2(mag.y, mag.x))
+    graph_yaw.plot(elapsed, yaw)
