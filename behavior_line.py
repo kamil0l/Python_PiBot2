ab731b8b6ccfb3805bce1781fc3457b66cb85a63
kamil <kamil_olszewski@op.pl>
2023-09-27 09:47:38 +0200

    all files final version

--- behavior_line.py
+++
@@ -1,9 +0,0 @@
-import robot
-from Raspi_MotorHAT import Raspi_MotorHAT
-from time import sleep
-
-r = robot.Robot()
-r.set_left(100)
-r.set_right(70)
-sleep(1)
-
--- robot_pose.py
+++
@@ -1,6 +0,0 @@
-import vpython as vp
-
-
-def robot_view():
-    vp.scene.forward = vp.vector(-3, -1, -1)
-    vp.scene.up = vp.vector(0, 0, 1)
